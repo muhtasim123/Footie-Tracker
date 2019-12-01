894f201f56d607005bdd258f2584f077b683afd3
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2019-12-01 13:10:10 -0500

    Completed set up of Player Activity, everything is done for this activity

--- app/src/main/java/com/example/footietracker/PlayerActivity.java
+++ app/src/main/java/com/example/footietracker/PlayerActivity.java
@@ -15,7 +15,7 @@ import org.w3c.dom.Text;
 public class PlayerActivity extends AppCompatActivity {
 
     Spinner spinner;
-    TextView playerName, team;
+    TextView playerName, team, position, age, number, nationality, goals, assists, shotAccuracy, passAccuracy;
     ImageView image;
 
     @Override
@@ -27,6 +27,15 @@ public class PlayerActivity extends AppCompatActivity {
         image = (ImageView) findViewById(R.id.imageView2);
         playerName = (TextView) findViewById(R.id.playerNameTextView);
         team = (TextView) findViewById(R.id.teamPlayedForTextView);
+        position = (TextView) findViewById(R.id.positionTextView);
+        age = (TextView) findViewById(R.id.ageTextView);
+        number = (TextView) findViewById(R.id.numberTextView);
+        nationality = (TextView) findViewById(R.id.nationalityTextView);
+        goals = (TextView) findViewById(R.id.goalsTextView);
+        assists = (TextView) findViewById(R.id.assistsTextView);
+        shotAccuracy = (TextView) findViewById(R.id.shotAccuracyTextView);
+        passAccuracy = (TextView) findViewById(R.id.passAccuracyTextView);
+
     }
 
     public void goHome(View v){
@@ -37,6 +46,15 @@ public class PlayerActivity extends AppCompatActivity {
     public void submit(View v){
         image.setImageResource(0);
         playerName.setText("");
+        team.setText("");
+        position.setText("");
+        age.setText("");
+        number.setText("");
+        nationality.setText("");
+        goals.setText("");
+        assists.setText("");
+        shotAccuracy.setText("");
+        passAccuracy.setText("");
 
         String name = "";
         if(spinner != null && spinner.getSelectedItem() !=null ) {
@@ -53,22 +71,67 @@ public class PlayerActivity extends AppCompatActivity {
             case "Lionel Messi":
                 image.setImageResource(R.drawable.lionelmessi);
                 playerName.setText(name);
+                team.setText("FC Barcelona");
+                position.setText("Forward");
+                age.setText("32");
+                number.setText("10");
+                nationality.setText("Argentine");
+                goals.setText("36");
+                assists.setText("14");
+                shotAccuracy.setText("62%");
+                passAccuracy.setText("81%");
                 break;
             case "Cristiano Ronaldo":
                 image.setImageResource(R.drawable.cristianoronaldo);
                 playerName.setText(name);
+                team.setText("Juventus");
+                position.setText("Forward");
+                age.setText("34");
+                number.setText("7");
+                nationality.setText("Portuguese");
+                goals.setText("21");
+                assists.setText("9");
+                shotAccuracy.setText("52%");
+                passAccuracy.setText("85%");
                 break;
             case "Neymar Da Silva":
                 image.setImageResource(R.drawable.neymardasilva);
                 playerName.setText(name);
+                team.setText("Paris St. Germain");
+                position.setText("Forward");
+                age.setText("27");
+                number.setText("10");
+                nationality.setText("Brazilian");
+                goals.setText("15");
+                assists.setText("7");
+                shotAccuracy.setText("62%");
+                passAccuracy.setText("81%");
                 break;
             case "Kylian Mbappe":
                 image.setImageResource(R.drawable.kylianmbappe);
                 playerName.setText(name);
+                team.setText("Paris St. Germain");
+                position.setText("Forward");
+                age.setText("20");
+                number.setText("7");
+                nationality.setText("French");
+                goals.setText("33");
+                assists.setText("7");
+                shotAccuracy.setText("67%");
+                passAccuracy.setText("79%");
                 break;
             case "Frankie De Jong":
                 image.setImageResource(R.drawable.frankiedejong);
                 playerName.setText(name);
+                team.setText("FC Barcelona");
+                position.setText("Midfielder");
+                age.setText("22");
+                number.setText("21");
+                nationality.setText("Dutch");
+                goals.setText("1");
+                assists.setText("2");
+                shotAccuracy.setText("100%");
+                passAccuracy.setText("93%");
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + name);
