76c0e31915d0d8cfcb9ff6da6baf325c0311886a
muhtasimchowdhury <muhtasim.chowdhury@uoit.net>
2019-12-01 12:32:09 -0500

    Completed set up of Team Activity, everything is done for this activity

--- app/src/main/java/com/example/footietracker/FootballApi.java
+++ app/src/main/java/com/example/footietracker/FootballApi.java
@@ -8,7 +8,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface FootballApi {
-    @Headers("X-RapidAPI-Key:0b8f6761bba462b1d96fd43fa237fe74")
+    @Headers("X-RapidAPI-Key: 0b8f6761bba462b1d96fd43fa237fe74")
     @GET("leagueTable/{league_id}")
     Call<List<Standings>> getStandings(@Path("league_id") int leagueID);
 }
